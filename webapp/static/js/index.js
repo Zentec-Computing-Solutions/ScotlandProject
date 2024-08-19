bb85e318d0161ec207b5084a82cd3518fbbcd98b
Bluemethyst <bluemethyst@gmail.com>
2024-08-19 13:39:41 +1200

    oh boy, back at it

--- webapp/static/js/index.js
+++ webapp/static/js/index.js
@@ -11,7 +11,9 @@ document.getElementById('flip-button').addEventListener('click', function() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(settings)  // Send the settings as JSON data in the POST request
-    });
+    }).then(r =>
+        console.log(JSON.stringify(r))
+    );
 });
 
 // Add more event listeners here for other buttons, sliders, checkboxes, etc.

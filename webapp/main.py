61d78c1557f298bef0a092bd5db9fa03c01a7f06
Bluemethyst <bluemethyst@gmail.com>
2024-09-16 14:46:41 +1200

    um

--- webapp/main.py
+++ webapp/main.py
@@ -5,7 +5,9 @@ from camera_handler import _gen_frames
 from loggerthyst import *
 from settings import _load_settings, _update_settings
 
-os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"  # https://www.reddit.com/r/learnpython/comments/zxxsal/comment/l5xscrp/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = (
+    "0"  # https://www.reddit.com/r/learnpython/comments/zxxsal/comment/l5xscrp/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+)
 import cv2
 
 app = Flask(__name__)
@@ -18,20 +20,22 @@ cap.set(cv2.CAP_PROP_FPS, 60)
 settings = _load_settings()
 
 
-@app.route('/update_settings', methods=['POST'])
+@app.route("/update_settings", methods=["POST"])
 def update_settings():
     return jsonify(_update_settings(settings))
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/raw_video')
+@app.route("/raw_video")
 def video_feed():
-    return Response(_gen_frames(cap, settings), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        _gen_frames(cap, settings), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=8080, host="0.0.0.0")

7ca6d53f8ded425f1c486b3192b698d978d0f8cb
Bluemethyst <bluemethyst@gmail.com>
2024-09-16 14:05:10 +1200

    wmop

--- webapp/main.py
+++ webapp/main.py
@@ -4,24 +4,30 @@ from io import BytesIO
 
 app = Flask(__name__)
 
+
 def gen(camera):
     stream = BytesIO()
-    for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+    for _ in camera.capture_continuous(stream, "jpeg", use_video_port=True):
         # Return current frame as response
         stream.seek(0)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
+        yield (
+            b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + stream.read() + b"\r\n"
+        )
         stream.seek(0)
         stream.truncate()
 
-@app.route('/video_feed')
+
+@app.route("/video_feed")
 def video_feed():
-    return Response(gen(PiCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        gen(PiCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ webapp/test.py
@@ -0,0 +1,5 @@
+from picamera import PiCamera
+
+camera = PiCamera()
+
+camera.capture("/home/pi/Desktop/image.jpg")
